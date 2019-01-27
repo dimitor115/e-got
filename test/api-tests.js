ba3c9f2e990239f61c7c9fa5c9d951be63127945
Maciej Kopeć <dimitor115@gmail.com>
2019-01-27 18:52:09 +0100

    add simple integration tests

--- test/api-tests.js
+++ test/api-tests.js
@@ -1,8 +1,37 @@
-const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+const axios = require('axios')
+const chai = require('chai')
+chai.should()
+
+const apiUrl = 'http://localhost:3000/api'
+
+describe('Integrations tests for e-got REST API', () => {
+
+  describe('Routes API', () => {
+    it('Should get list of suggested routes',async () => {
+      const result = await axios.get(`${apiUrl}/routes`)
+      result.data.should.have.length.at.least(1)
+    })
+  })
+
+  describe('Tracks API', () => {
+    it('Should add new track', () => {
+
+    })
+  })
+
+  describe('Evidences API', () => {
+    it('Should add new evidence', () => {
+
+    })
+
+    it('Should get list of evidences for tourist view', async () => {
+      const result = await axios.get(`${apiUrl}/evidences/tourist`)
+      result.data.should.have.length.at.least(1)
+    })
+
+    it('Should get list of evidences for verificator view', async () => {
+      const result = await axios.get(`${apiUrl}/evidences/verificator`)
+      result.data.should.have.length.at.least(1)
+    })
+  })
+})
